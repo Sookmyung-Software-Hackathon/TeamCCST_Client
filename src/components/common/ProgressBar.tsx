6e4c44e310a48acea406092c2a7363eae48b363e
sunny <huiseon37@gmail.com>
2022-08-28 20:13:44 +0900

    css: :art: progress 포지션 변경

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -14,7 +14,7 @@ export default ProgressBar;
 
 const Styled = {
   Root: styled.div`
-    position: absolute;
+    position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
